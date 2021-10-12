75b55e38332baafbe55b735fb3cc32b778ed6435
Ubuntu <ubuntu@ip-10-227-66-148.ec2.internal>
2021-10-12 06:20:47 +0000

    added files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Iac-eks
+# IaC-pipeline-example
+Infrastructure-as-Code pipeline example in aws.
+
+Run below to create EKS cluster.
+
+##
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
+
---
+++ security-groups.tf
@@ -0,0 +1,45 @@
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = var.vpc
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = var.vpc
+
+  ingress {
+    from_port = 3389
+    to_port   = 3389
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = var.vpc
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
